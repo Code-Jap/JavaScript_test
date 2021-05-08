6ed1e7ad48713e1af036c52870086ce09bb47805
Jasper <80272820+Code-Jap@users.noreply.github.com>
2021-05-08 21:06:28 +0200

    Add files via upload

---
+++ JavaScript_2/script.js
@@ -0,0 +1,12 @@
+console.log("Hallo Winc Academy studenten")
+
+/*
+
+// Dit is een grote som
+let multiplication = 1230941 * 1823794;
+console.log(multiplication);
+
+let division = 637263 / 54;
+console.log(division);
+
+*/
